92e2698b812ab4aac74361273e5da92bbbf5ded1
Stefano Miceli <mic.ninja@gmail.com>
2024-01-09 13:25:21 +0100

    adds examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+// import UseStateExample from "./components/UseStateExample";
+import UseEffectExample from "./components/UseEffectExample";
 
 function App() {
+  const [showEffectExample, setShowEffectExample] = useState(true);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setShowEffectExample(!showEffectExample)}>toggle showEffect</button>
+        {/* <UseStateExample /> */}
+        {showEffectExample && <UseEffectExample />}
       </header>
     </div>
   );
